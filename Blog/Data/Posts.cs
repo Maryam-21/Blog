b9fd9ea4232ce3b48d3f24ffabf7079482c41150
Maryam-21 <58235353+Maryam-21@users.noreply.github.com>
2021-04-13 11:59:46 +0200

    Add project files.

---
+++ Blog/Data/Posts.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Blog.Data
+{
+
+    public class Posts
+    {
+        public int id { get; set; }
+        public string author { get; set; }
+        public string content { get; set; }
+    }
+
+}
